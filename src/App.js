80a4c9076735ad80db3c0ad6af1a064d50c01b69
Moeez Rajpoot <99912138+Moeez-Rajpoot@users.noreply.github.com>
2023-08-22 23:39:56 +0500

    Updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
 
+  //other functions and hooks are declared and defined here
+
+ const [serachvalue, setserachvalue] = useState('');
+
+
+
+ const searchbox = (event) => {
+
+  let value = event.target.value.toLocaleLowerCase();
+  setserachvalue(value);
+  console.log({serachvalue});
+ 
+  
+ };
+
+
   return (
     <div className="App">
 
+
+      <input type="text" placeholder='Enter Monster name here'  onChange={searchbox} />
+    
   
     </div>
   );
